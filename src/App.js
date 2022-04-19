919ea82056f11f5fa0bee3e7e791f8818a05d647
Andrey Babkin <babkin.andrey@gmail.com>
2022-04-19 16:39:07 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/layout/Header';
 import Gallery from './components/layout/Gallery';
-import ItemDetails from './components/ItemDetails';
+import ItemDetails from './components/layout/ItemDetails';
 import AboutMe from './components/layout/AboutMe';
 import { Routes, Route } from 'react-router-dom';
 
