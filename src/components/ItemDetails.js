5b5729a4bc7a82f380e93c6950346c72a2b6f2c3
Andrey Babkin <babkin.andrey@gmail.com>
2022-04-19 15:22:42 +0300

    created spinner

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,48 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Button, Col, Container, Figure, Row } from 'react-bootstrap';
+import { Button, Col, Container, Figure, Row, Spinner } from 'react-bootstrap';
 import axios from 'axios';
 
 const ItemDetails = () => {
-  let {itemId}= useParams();
+  let { itemId } = useParams();
   let navigate = useNavigate();
   const [itemData, setItemData] = useState({});
+  const [isLoaderShown, setIsLoaderShown] = useState(false);
 
-
-  useEffect(() =>{
+  useEffect(() => {
     const apiUrl = `https://jsonplaceholder.typicode.com/photos/${itemId}`;
+    setIsLoaderShown(true);
     axios.get(apiUrl).then((resp) => {
       const imageData = resp.data;
       setItemData(imageData);
-    })
-  }, [itemId])
+    }).then(()=> {
+      setIsLoaderShown(false)
+    });
+
+  }, [itemId]);
+
   return (
     <section className="singleGalleryItem mt-5">
       <Container>
         <Row>
-          <Col xs={12} md={6}>
-            <div className="singleGalleryItem__image">
-              <Figure>
-                <Figure.Image src={itemData.url} />
-              </Figure>
-            </div>
-          </Col>
-          <Col xs={12} md={6}>
-            <div className="singleGalleryItem__info">
-              <p>
-                <span className="fw-bold">Id:</span> {itemData.id}
-              </p>
-              <p>
-                <span className="fw-bold">Title: </span>
-                {itemData.title}
-              </p>
-              <p>
-                <span className="fw-bold">Link:</span>{' '}
-                <a href={itemData.url}>{itemData.url}</a>
-              </p>
-              <Button variant="primary" onClick={() =>{navigate(-1)}}>Назад</Button>
-            </div>
-          </Col>
+          {isLoaderShown ? (
+            <Spinner animation="border" variant="primary" className="mt-3" />
+          ) : (
+            <>
+              <Col xs={12} md={6}>
+                <div className="singleGalleryItem__image">
+                  <Figure>
+                    <Figure.Image src={itemData.url} />
+                  </Figure>
+                </div>
+              </Col>
+              <Col xs={12} md={6}>
+                <div className="singleGalleryItem__info">
+                  <p>
+                    <span className="fw-bold">Id:</span> {itemData.id}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Title: </span>
+                    {itemData.title}
+                  </p>
+                  <p>
+                    <span className="fw-bold">Link:</span>{' '}
+                    <a href={itemData.url}>{itemData.url}</a>
+                  </p>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      navigate(-1);
+                    }}
+                  >
+                    Назад
+                  </Button>
+                </div>
+              </Col>
+            </>
+          )}
         </Row>
       </Container>
     </section>
